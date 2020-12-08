363f643ee1bb8064180d500d4c71069287f64ee8
Sasi_official <42794655+Sasi-007@users.noreply.github.com>
2020-12-08 14:12:33 +0530

    Create CountingValleys.cpp

---
+++ CountingValleys.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    char a;
+
+    int con = 0, total = 0;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+
+        if(a == 'U')
+            con++;
+        if(a == 'D'){
+            con--;
+            if(con == -1)
+                total++;
+        }
+
+    }
+    cout << total << endl;
+
+    return 0;
+}
